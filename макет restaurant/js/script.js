2c5a79658731788120b726f2092b27e9e9866888
HarDaster <malsagov.1987@mail.ru>
2019-04-03 00:22:09 +0400

    Comit First

---
+++ макет restaurant/js/script.js
@@ -0,0 +1,12 @@
+window.onload = function () {
+	var menuStyle = getComputedStyle (menu);
+	openMenu.onclick = function () {
+		if(menuStyle.display == "none") {
+			menu.classList.add("active");
+			this.innerHTML = "Close menu";
+		} else {
+			menu.classList.remove("active");
+			this.innerHTML = "Open menu";
+		}
+	}
+}
