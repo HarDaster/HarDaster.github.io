fe36b66d3ab82392f5585e330184d74071c0f4b4
HarDaster <40760289+HarDaster@users.noreply.github.com>
2019-10-20 10:51:19 +0300

    Add files via upload

---
+++ tinyone/js/script.js
@@ -0,0 +1,43 @@
+$(document).ready(function(){
+  $('.burger').click(function(){
+  $('ul').toggleClass('active');
+  })
+})
+
+
+$(function(){
+  $('a[href^="#"]').on('click', function(event) {
+    // отменяем стандартное действие
+    event.preventDefault();
+    
+    var sc = $(this).attr("href"),
+        dn = $(sc).offset().top;
+    /*
+    * sc - в переменную заносим информацию о том, к какому блоку надо перейти
+    * dn - определяем положение блока на странице
+    */
+    
+    $('html, body').animate({scrollTop: dn}, 1000);
+    
+    /*
+    * 1000 скорость перехода в миллисекундах
+    */
+  });
+});
+
+function backToTop() {
+  let button = $('.btn_top');
+  $(window).on('scroll', () => {
+    if ($(this).scrollTop() >= 50) {
+      button.fadeIn();
+    } else {
+      button.fadeOut();
+    }
+  });
+  button.on('click', (e) => {
+    e.preventDefault();
+    $('html').animate({scrollTop: 0}, 1000);
+  })
+}
+
+backToTop();
